705a0f060d4714037d914b6bb73849b551a7fb4f
Ilyan_69 <113367728+Ilyan-saib@users.noreply.github.com>
2022-10-08 22:38:03 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let toDoContainer = document.getElementById('toDoContainer')
+let addToDobutton = document.getElementById('addToDo')
+let inputField = document.getElementById('inputField')
+
+addToDobutton.addEventListener('click' , function() {
+    var paragraph = document.createElement('p');
+   paragraph.innerText = inputField.value;
+   paragraph.classList.add('paragraph-styling')
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration ="line-through";
+    })
+
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
